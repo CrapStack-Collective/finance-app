9bf02696e1336b6e9395c2e94e5a067a91d92ea4
Marvin Pahling <marvin@Marvins-MacBook-Pro.local>
2025-03-18 13:42:58 +0100

    add frontend

---
+++ apps/frontend/app/ssr.tsx
@@ -0,0 +1,11 @@
+import { getRouterManifest } from "@tanstack/react-start/router-manifest";
+import { createRouter } from "./router";
+import {
+  createStartHandler,
+  defaultStreamHandler,
+} from "@tanstack/react-start/server";
+
+export default createStartHandler({
+  createRouter,
+  getRouterManifest,
+})(defaultStreamHandler);
---
+++ apps/frontend/index.ts
@@ -0,0 +1,1 @@
+console.log("the fronend is working");
